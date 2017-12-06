838e95ac9b1586874d3dd3610a9c03fcf74f6ead
kk9923 <317168664@qq.com>
2017-12-06 23:50:37 +0800

    属性动画 - 花束直播加载效果

---
+++ day18/src/main/java/com/kx/admin/day18/LoveLayout.java
@@ -0,0 +1,24 @@
+package com.kx.admin.day18;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by admin on 2017/12/6.
+ * 贝塞尔曲线 - 花束直播点赞效果
+ */
+
+public class LoveLayout extends RelativeLayout {
+    public LoveLayout(Context context) {
+        super(context);
+    }
+
+    public LoveLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoveLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
